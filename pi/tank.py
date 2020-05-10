ca066eba3cae84adc465833da6679b50a085162d
Bryce Wade <bryce.wade@pearson.com>
2020-05-09 22:27:10 -0500

    Add hold heading button

--- pi/tank.py
+++ pi/tank.py
@@ -8,6 +8,7 @@ RIGHT_JOYSTICK = 4
 L1_BUTTON = 4
 R1_BUTTON = 5
 X_BUTTON = 0
+TRIANGLE_BUTTON = 2
 PS_BUTTON = 10
 ARDUINO = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Mega_2560_55838323735351102081-if00"
 
@@ -16,21 +17,30 @@ def start_serial():
     arduino = serial.Serial(ARDUINO, 115200, timeout=0)
     return arduino
 
+
+def read_serial(arduino):
+    return_message = arduino.read(32767)
+    while return_message:
+        print(return_message.decode(), end ="")
+        return_message = arduino.read(32767)
+
+
 def send_to_arduino(arduino, message):
     # print(" " + message)
     message_bytes=message.encode() + b'\r'
     # for i in message_bytes:
     #     print(i)
     arduino.write(message_bytes)
-    return_message = arduino.read(32767)
-    while return_message:
-        print(return_message.decode())
-        return_message = arduino.read(32767)
+    read_serial(arduino)
 
 
 def record_position(arduino):
     send_to_arduino(arduino, "M114")
     # location=read_from_arduino()
+
+
+def hold_heading(arduino):
+    send_to_arduino(arduino, "G1002")
 
 
 def send_updated_directions(arduino, left, right):
@@ -64,12 +74,20 @@ def main():
         left = joystick.get_axis(LEFT_JOYSTICK)
         right = joystick.get_axis(RIGHT_JOYSTICK)
         x_button = joystick.get_button(X_BUTTON)
+        tri_button = joystick.get_button(TRIANGLE_BUTTON)
+        x_button = joystick.get_button(X_BUTTON)
         l1_button = joystick.get_button(L1_BUTTON)
         r1_button = joystick.get_button(R1_BUTTON)
         if x_button and not previous_x:
             print("Recording position")
             record_position(arduino)
         previous_x = x_button
+        
+        if tri_button and not previous_tri:
+            print("Holding current heading")
+            hold_heading(arduino)
+        previous_tri = tri_button
+
 
         if l1_button and not previous_l1:
             left_blade_active = not left_blade_active
@@ -96,6 +114,7 @@ def main():
         if joystick.get_button(PS_BUTTON) == 1:
             print("Exiting.")
             done = True
+        read_serial(arduino)
 
 if __name__ == "__main__":
     # execute only if run as a script
